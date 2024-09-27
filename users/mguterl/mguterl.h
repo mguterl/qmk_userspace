b92a81d35ec1f02f5f9d1e7f134b0075b82fb299
Mike Guterl <mike@diminishing.org>
2024-09-27 09:29:53 -0700

    Add gaming layout

--- users/mguterl/mguterl.h
+++ users/mguterl/mguterl.h
@@ -10,11 +10,15 @@
 /* Control when held, Escape when tapped */
 #define ESC_CTL MT(MOD_LCTL, KC_ESC)
 
+/* Disable keybinds for better gaming */
+#define TG_GAME TG(_GAMING)
+
 /*
  * Common layers and keycodes that are shared between Planck and Preonic
  */
 enum userspace_layers {
   _QWERTY,
+  _GAMING,
   _FN,
   _NAV
 };
